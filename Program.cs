a45b6bc7ff1cb4ca36c4199cba1cdbc59aae7a71
yvariava <99268849+yvariava@users.noreply.github.com>
2024-09-26 18:13:46 +0100

    Update Program.cs
    Changed the main cs file so that the other problems can be ran as separate classes.

--- Program.cs
+++ Program.cs
@@ -1,21 +1,11 @@
-﻿namespace BIO_2013;
+using static BIO_2013.Problem1;
+namespace BIO_2013;
 class Program
 {
     static void Main(string[] args)
     {
-        Problem1();
-    }
-
-    static void Problem1()
-    {
-        int[] inputTimes = Console.ReadLine().Split(' ').Select(x => int.Parse(x)+60).ToArray();
-        TimeSpan currentTime0 = TimeSpan.FromMinutes(inputTimes[0]);
-        TimeSpan currentTime1 = TimeSpan.FromMinutes(inputTimes[1]);
-        while (currentTime0.Hours != currentTime1.Hours || currentTime0.Minutes != currentTime1.Minutes)
-        {
-            currentTime0 += TimeSpan.FromMinutes(inputTimes[0]);
-            currentTime1 += TimeSpan.FromMinutes(inputTimes[1]);
-        }
-        Console.WriteLine(currentTime0.ToString("hh':'mm"));
+        SolveProblem1A();
+        //SolveProblem1B();
+        
     }
 }
